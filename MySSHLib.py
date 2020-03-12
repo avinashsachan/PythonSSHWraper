ee40882caad13af729a02f3bf34b60d35ce1dc7e
Avinash <avinashsachan@live.in>
2020-03-12 07:00:16 +0530

    p3

--- MySSHLib.py
+++ MySSHLib.py
@@ -6,7 +6,14 @@ import threading
 
 
 class MySSHLib:
-    def __init__(self, ip, port, username, password, timeout: int = 30):
+    def __init__(self, ip: str, username: str, password: str, port: int = 22, timeout: int = 30):
+        self.logger = logging.getLogger("MySSHLib")
+        self.logger.setLevel(level=logging.ERROR)
+
+        ch = logging.StreamHandler()
+        ch.setLevel(level=logging.DEBUG)
+        self.logger.addHandler(ch)
+
         self.ip = ip
         self.port = port
         self._username = username
@@ -35,6 +42,7 @@ class MySSHLib:
             x = self.channel.recv(1024 * 10)
             if x != b'':
                 z = x.decode("utf-8")
+                self.logger.debug(z)
                 self.sessionLog += z
                 self.bufferLog = ((self.bufferLog + z)[-50:]).lower()
                 t = threading.Thread(target=self.onReceiveData)
@@ -45,11 +53,13 @@ class MySSHLib:
     def connect(self, connectTimeout=10, serverPrompt="$|#", separator="|", term="dumb"):
         try:
             term = term if term in ["vt100", "dumb"] else "dumb"
-            self.client.connect(self.ip, port=self.port, username=self.username, password=self.password,
+            self.client.connect(self.ip, port=self.port, username=self._username, password=self._password,
                                 look_for_keys=False, timeout=connectTimeout)
             self.channel = self.client.invoke_shell(term=term, width=1600, height=1200)
             t = threading.Thread(target=self.onReceiveData)
             t.start()
+            if serverPrompt is not None:
+                self.wait(serverPrompt, separator)
         except Exception as e:
             raise e
 
@@ -70,3 +80,17 @@ class MySSHLib:
             raise TimeoutError("Exit not found")
         except:
             pass
+
+    def sendMessage(self, command, suppressCR=False):
+        if suppressCR:
+            self.channel.send(command)
+        else:
+            self.channel.send(command + "\r")
+
+    def sendAndWait(self, command, waitFor, breakCharacter, suppressCR=False):
+        self.sendMessage(command, suppressCR)
+        self.wait(waitFor, breakCharacter)
+
+    def clearSessionLog(self):
+        self.sessionLog = ""
+        self.bufferLog = ""
