3ce88b264f1778c06abfcaad095a18051956c8f4
Avinash <avinashsachan@live.in>
2020-03-12 20:00:13 +0530

    p3

--- MySSHLib.py
+++ MySSHLib.py
@@ -8,8 +8,7 @@ import threading
 class MySSHLib:
     def __init__(self, ip: str, username: str, password: str, port: int = 22, timeout: int = 30):
         self.logger = logging.getLogger("MySSHLib")
-        self.logger.setLevel(level=logging.ERROR)
-
+        self.logger.setLevel(level=logging.DEBUG)
         ch = logging.StreamHandler()
         ch.setLevel(level=logging.DEBUG)
         self.logger.addHandler(ch)
@@ -19,12 +18,11 @@ class MySSHLib:
         self._username = username
         self._password = password
         self.client = client.SSHClient()
-        self.client.load_system_host_keys()
-        self.client.set_missing_host_key_policy(client.AutoAddPolicy())
         self.channel = channel.Channel(chanid=100)
         self.sessionLog = ""
-        self.bufferLog = ""
+        self._bufferLog = ""
         self.timeout = timeout
+        self._bufferSize = 1024 * 122
 
     def __del__(self):
         self.disconnect()
@@ -39,27 +37,31 @@ class MySSHLib:
     def onReceiveData(self):
         try:
             if self.client is None or self.channel is None: return
-            x = self.channel.recv(1024 * 10)
-            if x != b'':
+            x = self.channel.recv(self._bufferSize)
+            if x is not None and x != b'':
                 z = x.decode("utf-8")
                 self.logger.debug(z)
                 self.sessionLog += z
-                self.bufferLog = ((self.bufferLog + z)[-50:]).lower()
+                self._bufferLog = ((self._bufferLog + z)[-50:]).lower()
                 t = threading.Thread(target=self.onReceiveData)
                 t.start()
         except:
             pass
 
-    def connect(self, connectTimeout=10, serverPrompt="$|#", separator="|", term="dumb"):
+    def connect(self, connectTimeout=10, serverPrompt="$|#", breakCharacter="|", term="vt100"):
         try:
             term = term if term in ["vt100", "dumb"] else "dumb"
+            self.client.load_system_host_keys()
+            self.client.set_missing_host_key_policy(client.AutoAddPolicy())
             self.client.connect(self.ip, port=self.port, username=self._username, password=self._password,
                                 look_for_keys=False, timeout=connectTimeout)
+            self.logger.debug("connected")
             self.channel = self.client.invoke_shell(term=term, width=1600, height=1200)
             t = threading.Thread(target=self.onReceiveData)
             t.start()
             if serverPrompt is not None:
-                self.wait(serverPrompt, separator)
+                self.wait(serverPrompt, breakCharacter)
+
         except Exception as e:
             raise e
 
@@ -74,9 +76,8 @@ class MySSHLib:
             while datetime.now() <= dt:
                 time.sleep(.05)
                 for i in range(0, len(split_part)):
-                    if split_part[i] in self.bufferLog:
+                    if split_part[i] in self._bufferLog:
                         return i
-
             raise TimeoutError("Exit not found")
         except:
             pass
@@ -88,9 +89,29 @@ class MySSHLib:
             self.channel.send(command + "\r")
 
     def sendAndWait(self, command, waitFor, breakCharacter, suppressCR=False):
+        self._bufferLog = ""
         self.sendMessage(command, suppressCR)
-        self.wait(waitFor, breakCharacter)
+        return self.wait(waitFor, breakCharacter)
 
     def clearSessionLog(self):
         self.sessionLog = ""
-        self.bufferLog = ""
+        self._bufferLog = ""
+
+    # def sendCommand(self, command):
+    #     # Check if connection is made previously
+    #     print("1")
+    #     if self.client:
+    #         stdin, stdout, stderr = self.client.exec_command(command)
+    #         while not stdout.channel.exit_status_ready():
+    #             # Print stdout data when available
+    #             if stdout.channel.recv_ready():
+    #                 # Retrieve the first 1024 bytes
+    #                 alldata = stdout.channel.recv(1024)
+    #                 while stdout.channel.recv_ready():
+    #                     # Retrieve the next 1024 bytes
+    #                     alldata += stdout.channel.recv(1024)
+    #
+    #                 # Print as string with utf8 encoding
+    #                 print(12,str(alldata, "utf8"))
+    #     else:
+    #         print("Connection not opened.")
